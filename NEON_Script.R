9bcf9eb83be028945f6ab09f3a8c915aad1a3a13
ecrispo@pace.edu <ecrispo@pace.edu>
2020-06-10 15:32:24 -0400

    June 10

--- NEON_Script.R
+++ NEON_Script.R
@@ -10,3 +10,12 @@ summary(mosquito)
 
 #A few missing steps first
 t.test(mosquito$fishTotalLength~mosquito$before_after)
+
+#calculate means and s for each of the two groups (before and after)
+aggregate(mosquito$fishTotalLength,by=list(mosquito$before_after),FUN=mean)
+aggregate(mosquito$fishTotalLength,by=list(mosquito$before_after),FUN=sd)
+aggregate(mosquito$fishTotalLength,by=list(mosquito$before_after),FUN=length)
+
+
+tapply(mosquito$fishTotalLength, mosquito$before_after, sd)
+tapply(mosquito$fishTotalLength, mosquito$before_after, var)
