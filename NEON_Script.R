c69f652f7000758656474b154981138575e3bf76
shayansaniei <shayan.saniei@gmail.com>
2020-06-16 20:39:05 -0400

    June 16th

--- NEON_Script.R
+++ NEON_Script.R
@@ -7,9 +7,14 @@ fish$SPECIES<-as.factor(fish$SPECIES)
 fish$before_after<-as.factor(fish$before_after)
 mosquito<-subset(fish,fish$SPECIES=="Gambusia_affinis")
 summary(mosquito)
-
-#A few missing steps first
-t.test(mosquito$fishTotalLength~mosquito$before_after)
+#plot the histograms
+hist(mosquito$fishTotalLength[mosquito$before_after=="before"],breaks=16,col="red",xlab="Fish total lenght(mm)",ylab="Number of fish",main="Fish lenght distribution (before)")
+hist(mosquito$fishTotalLength[mosquito$before_after=="after"],breaks=16,col="red",xlab="Fish total lenght(mm)",ylab="Number of fish",main="Fish lenght distribution (after)")
+#standard deviation
+sd(mosquito$fishTotalLength[mosquito$before_after=="before"])
+sd(mosquito$fishTotalLength[mosquito$before_after=="after"])
+#Welsh t-test
+t.test(mosquito$fishTotalLength~mosquito$before_after, data=mosquito, var.equal=F)
 
 #calculate means and s for each of the two groups (before and after)
 aggregate(mosquito$fishTotalLength,by=list(mosquito$before_after),FUN=mean)
@@ -21,3 +26,6 @@ tapply(mosquito$fishTotalLength, mosquito$before_after, sd)
 tapply(mosquito$fishTotalLength, mosquito$before_after, var)
 #Checking
 print("fish")
+#Next species
+Xiphophorus<-subset(fish,fish$SPECIES=="Xiphophorus_hellerii")
+summary(Xiphophorus)
