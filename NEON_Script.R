7ae0961c0fd2b73c3afca55443d810dd292147e0
shayansaniei <shayan.saniei@gmail.com>
2020-08-19 19:00:40 -0400

    last edit

--- NEON_Script.R
+++ NEON_Script.R
@@ -113,7 +113,13 @@ summary(fit2)
 interaction.plot(SITE, before_after, Poecilia$fishWeight, type="b", col=c("red","blue"), pch=c(16, 18), main="Interaction between hurricane and site", ylab= "Poecilia fish mean weight")
 
 PoeciliaCUPE<-subset(Poecilia,Poecilia$SITE=="CUPE")
+PoeciliaCUPE$DATE<-as.Date(PoeciliaCUPE$DATE)
+qplot(x=DATE,y=fishTotalLength, 
+      data=PoeciliaCUPE)
 PoeciliaGUIL<-subset(Poecilia,Poecilia$SITE=="GUIL")
+PoeciliaGUIL$DATE<-as.Date(PoeciliaGUIL$DATE)
+qplot(x=DATE,y=fishTotalLength, 
+      data=PoeciliaGUIL)
 summary(PoeciliaCUPE)
 summary(PoeciliaGUIL)
 length(PoeciliaCUPE)
@@ -180,6 +186,9 @@ t.test(logweightgobies~gobies$before_after, data=gobies, var.equal=F)
 
 #Focus on the spotted algae eating goby ONLY
 spottedGoby<-subset(fish,fish$SPECIES=="Sicydium_punctatum")
+spottedGoby$DATE<-as.Date(spottedGoby$DATE)
+qplot(x=DATE,y=fishTotalLength, data=spottedGoby) 
+summary(spottedGoby)
 length(spottedGoby$fishWeight)
 hist(spottedGoby$fishTotalLength[spottedGoby$before_after=="before"],breaks=16,col="red",xlab="Fish length (mm)",ylab="Number of fish",main="Fish length distribution (before)")
 hist(spottedGoby$fishTotalLength[spottedGoby$before_after=="after"],breaks=16,col="red",xlab="Fish length (mm)",ylab="Number of fish",main="Fish length distribution (after)")
@@ -225,14 +234,28 @@ qplot(x=DATE,y=waterTemp,
       data=EnvCUPE,
       main="water temp Cupe",xlab="Year",ylab="Water Temp (C)")  
 qplot(x=DATE,y=pH, data=EnvCUPE,main="water pH")
+
+TempCUPE <- ggplot(EnvCUPE,aes(x=DATE,y=waterTemp))+labs(x="Year",y="Water temp (C)")+ geom_point()
+TempCUPE
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+TempCUPE + geom_vline(xintercept=dates_vline, linetype="dashed")
 #removing the outlier
 EnvCUPE.pH<-subset(Env,Env$SITE=="CUPE" & Env$pH>5)
 EnvCUPE.pH$DATE<-as.Date(EnvCUPE.pH$DATE)
 qplot(x=DATE,y=pH, data=EnvCUPE.pH,main="water Ph",xlab="year")
+pHCUPE <- ggplot(EnvCUPE.pH,aes(x=DATE,y=pH))+labs(x="Year",y="pH")+ geom_point()
+pHCUPE
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+pHCUPE + geom_vline(xintercept=dates_vline, linetype="dashed")
 
 
 qplot(x=DATE,y=dissolvedOxygen,data=EnvCUPE,main="dissolved Oxygen",ylab="Dissolved Oxygen (mg/L)",xlab="Year")
 qplot(x=DATE,y=dissolvedOxygenSaturation,data=EnvCUPE,main="dissloved Oxygen saturation")
+OxyCUPE <- ggplot(EnvCUPE,aes(x=DATE,y=dissolvedOxygen))+labs(x="Year",y="Dissolved oxygen (mg/L)") + geom_point()
+OxyCUPE
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+OxyCUPE + geom_vline(xintercept=dates_vline, linetype="dashed")
+
 
 EnvGUIL<-subset(Env, Env$SITE=="GUIL")
 EnvGUIL$DATE<-as.Date(EnvGUIL$DATE)
@@ -243,27 +266,44 @@ qplot(x=DATE,y=waterTemp,
 EnvGUIL.temp<-subset(Env,Env$SITE=="GUIL" & Env$waterTemp>15)
 EnvGUIL.temp$DATE<-as.Date(EnvGUIL.temp$DATE)
 qplot(x=DATE,y=waterTemp, data=EnvGUIL.temp,main="water temp",xlab="Year",ylab="Water Temp (C)")
+TempGUIL <- ggplot(EnvGUIL.temp,aes(x=DATE,y=waterTemp))+labs(x="Year",y="Water temp (C)") + geom_point()
+TempGUIL
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+TempGUIL + geom_vline(xintercept=dates_vline, linetype="dashed")
 
 qplot(x=DATE,y=pH, data=EnvGUIL,main="water pH")
 #removing pH outlier
 EnvGUIL.pH<-subset(Env,Env$SITE=="GUIL" & Env$pH>5)
 EnvGUIL.pH$DATE<-as.Date(EnvGUIL.pH$DATE)
 qplot(x=DATE,y=pH,data=EnvGUIL.pH,main="water pH",xlab="Year",ylab="pH")
+pHGUIL <- ggplot(EnvGUIL.pH,aes(x=DATE,y=pH))+labs(x="Year",y="pH") + geom_point()
+pHGUIL
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+pHGUIL + geom_vline(xintercept=dates_vline, linetype="dashed")
+
 
 qplot(x=DATE,y=dissolvedOxygen,data=EnvGUIL,main="dissolved Oxygen",xlab="Year",ylab="Dissolved Oxygen (mg/L)")
 
 qplot(x=DATE,y=dissolvedOxygenSaturation,data=EnvGUIL,main="dissloved Oxygen saturation")
-
+OxyGUIL <- ggplot(EnvGUIL,aes(x=DATE,y=dissolvedOxygen))+labs(x="Year",y="Dissolved oxygen (mg/L)") + geom_point()
+OxyGUIL
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+OxyGUIL + geom_vline(xintercept=dates_vline, linetype="dashed")
 
 #New analysis
 
 
 Xiphophorus$DATE
 Xiphophorus$DATE<-as.Date(Xiphophorus$DATE)
+
 qplot(x=DATE,y=fishTotalLength, data=Xiphophorus) 
 #Added this one:
 Xiphophorus2$DATE<-as.Date(Xiphophorus2$DATE)
 qplot(x=DATE,y=fishTotalLength, data=Xiphophorus2) 
+xiph <- ggplot(Xiphophorus2,aes(x=DATE,y=fishTotalLength))+labs(x="Year",y="Fish length (mm)")+ geom_point()
+xiph
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+xiph + geom_vline(xintercept=dates_vline, linetype="dashed")
 #I was worried that the data were bimodal because you have data from two different months in "after"
 #But even when you look only at Feb, the "after" data are biomodal. 
 hist(Xiphophorus2$fishTotalLength[Xiphophorus2$DATE > "2017-12-07"])
@@ -287,7 +327,7 @@ ggplot(Xiphophorus2, aes(x=before_after, y=fishTotalLength)) +
                color="black", notch=TRUE)+ geom_point(position="jitter")
 ggplot(Xiphophorus2, aes(before_after, y=fishTotalLength)) +
   geom_violin(fill="white") + geom_boxplot(fill="white", width=.2)+
-  labs(x="Timing of data collection reletive to hurricanes",y="Total Fish Length (mm)")
+  labs(x="Timing of data collection relative to hurricanes",y="Total Fish Length (mm)")
 #It is easy to see the bimodal distribution in the violin plot. Easier than in the other two plots.
 
 Agonostomus$DATE
@@ -296,6 +336,11 @@ Agonostomus2
 Agonostomus2$DATE<-as.Date(Agonostomus2$DATE)
 qplot(x=DATE,y=fishTotalLength, 
       data=Agonostomus2)
+agon <- ggplot(Agonostomus2,aes(x=DATE,y=fishTotalLength))+labs(x="Year",y="Fish length (mm)")+ geom_point()
+agon
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+agon + geom_vline(xintercept=dates_vline, linetype="dashed")
+
 hist(Agonostomus2$fishTotalLength[Agonostomus2$before_after=="before"],breaks=16,col="red",xlab="Fish total length(mm)",ylab="Number of fish",main="Fish lenght distribution (before)")
 #This one below also looks a bit bimodal:
 hist(Agonostomus2$fishTotalLength[Agonostomus2$before_after=="after"],breaks=16,col="red",xlab="Fish total length(mm)",ylab="Number of fish",main="Fish lenght distribution (after)")
@@ -309,7 +354,7 @@ ggplot(Agonostomus2, aes(x=before_after, y=fishTotalLength)) +
                color="black", notch=TRUE)+ geom_point(position="jitter")
 ggplot(Agonostomus2, aes(before_after, y=fishTotalLength)) +
   geom_violin(fill="white") + geom_boxplot(fill="white", width=.2)+
-  labs(x="Timing of data collection reletive to hurricanes",y="Total Fish Length (mm)")
+  labs(x="Timing of data collection relative to hurricanes",y="Total Fish Length (mm)")
 #I am doing this out of curiosity:
 wilcox.test(Agonostomus2$fishTotalLength~Agonostomus2$before_after)
 #P values are typically lower for the t-test than for the rank-based test
@@ -321,6 +366,10 @@ wilcox.test(Agonostomus2$fishTotalLength~Agonostomus2$before_after)
 mosquito$DATE<-as.Date(mosquito$DATE)
 qplot(x=DATE,y=fishTotalLength, 
       data=mosquito)
+mosq <- ggplot(mosquito,aes(x=DATE,y=fishTotalLength))+labs(x="Year",y="Fish Length (mm)")+ geom_point()
+mosq
+dates_vline <- as.Date(c("2017-09-06", "2017-09-20"))  
+mosq + geom_vline(xintercept=dates_vline, linetype="dashed")
 hist(mosquito$fishTotalLength[mosquito$before_after=="before"],breaks=16,col="red",xlab="Fish total lenght(mm)",ylab="Number of fish",main="Fish lenght distribution (before)")
 hist(mosquito$fishTotalLength[mosquito$before_after=="after"],breaks=16,col="red",xlab="Fish total lenght(mm)",ylab="Number of fish",main="Fish lenght distribution (after)")
 tapply(mosquito$fishTotalLength, mosquito$before_after, mean)     
@@ -334,7 +383,7 @@ ggplot(mosquito, aes(x=before_after, y=fishTotalLength)) +
                color="black", notch=TRUE)+ geom_point(position="jitter")
 ggplot(mosquito, aes(before_after, y=fishTotalLength)) +
   geom_violin(fill="white") + geom_boxplot(fill="white", width=.2)+
-  labs(x="Timing of data collection reletive to hurricanes",y="Total Fish Length (mm)")
+  labs(x="Timing of data collection relative to hurricanes",y="Total Fish Length (mm)")
 #If you want to strive for consistency, it would be valid to use Mann-Whitney-Wilcoxon test for all three analyses
 #It would not change any of your conclusions
 
